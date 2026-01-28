8cf853bfdb12726e76209e352d12ea7ce0de7568
devikapillai30 <devikapillai145@gmail.com>
2026-01-28 10:23:16 -0500

    Updating files to deployed bot
    Signed-off-by: devikapillai30 <devikapillai145@gmail.com>

---
+++ src/config.py
@@ -0,0 +1,17 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    """Bot Configuration"""
+
+    PORT = 3978
+    APP_ID = os.environ.get("CLIENT_ID", "")
+    APP_PASSWORD = os.environ.get("CLIENT_SECRET", "")
+    APP_TYPE = os.environ.get("BOT_TYPE", "")
+    APP_TENANTID = os.environ.get("TENANT_ID", "")
+    AZURE_OPENAI_API_KEY = os.environ["AZURE_OPENAI_API_KEY"] # Azure OpenAI API key
+    AZURE_OPENAI_MODEL_DEPLOYMENT_NAME = os.environ["AZURE_OPENAI_MODEL_DEPLOYMENT_NAME"] # Azure OpenAI model deployment name
+    AZURE_OPENAI_ENDPOINT = os.environ["AZURE_OPENAI_ENDPOINT"] # Azure OpenAI endpoint
